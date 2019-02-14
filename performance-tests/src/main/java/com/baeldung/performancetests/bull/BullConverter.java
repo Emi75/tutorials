1eb18bb80d6f0e64932a6b0abf7f4f6243e08317
elandolfi <elandolfi@expedia.com>
2019-02-14 13:02:22 +0100

    - Added the exec-maven-plugin to allow the test execution from the command line, so independent from the IDE - Bumped the maven-compiler-plugin.version to the latest - Added the jmh-generator-annprocess as workaround for the bug related to the maven-compiler-plugin - Integrated the Bean Utils Library into the performance testing project - Modified the Review destination object as workaround for the bug related to the FieldTransformer

---
+++ performance-tests/src/main/java/com/baeldung/performancetests/bull/BullConverter.java
@@ -0,0 +1,40 @@
+package com.baeldung.performancetests.bull;
+
+import com.baeldung.performancetests.Converter;
+import com.baeldung.performancetests.model.destination.DestinationCode;
+import com.baeldung.performancetests.model.source.AccountStatus;
+import com.baeldung.performancetests.model.source.OrderStatus;
+import com.baeldung.performancetests.model.source.PaymentType;
+import com.baeldung.performancetests.model.source.SourceCode;
+import com.baeldung.performancetests.model.source.SourceOrder;
+import com.baeldung.performancetests.model.destination.Order;
+import com.hotels.beans.BeanUtils;
+import com.hotels.beans.model.FieldMapping;
+import com.hotels.beans.model.FieldTransformer;
+
+    public class BullConverter implements Converter {
+        private final BeanUtils beanUtils;
+
+        public BullConverter() {
+            beanUtils = new BeanUtils();
+        }
+
+        @Override
+        public Order convert(SourceOrder sourceOrder) {
+            FieldTransformer<AccountStatus, com.baeldung.performancetests.model.destination.AccountStatus> accountStatusTransformer = 
+                    new FieldTransformer<>("userAccountStatus", userAccountStatus -> new com.baeldung.performancetests.model.destination.User().conversion(sourceOrder.getOrderingUser().getUserAccountStatus()));
+            FieldTransformer<OrderStatus, com.baeldung.performancetests.model.destination.OrderStatus> orderStatusTransformer = 
+                    new FieldTransformer<>("orderStatus", orderStatus -> new Order().conversion(sourceOrder.getStatus()));
+            FieldTransformer<PaymentType, com.baeldung.performancetests.model.destination.PaymentType> paymentTypeTransformer = 
+                    new FieldTransformer<>("paymentType", paymentType -> new Order().conversion(sourceOrder.getPaymentType()));
+            return beanUtils.getTransformer()
+                    .withFieldMapping(new FieldMapping("status", "orderStatus"))
+                    .withFieldTransformer(accountStatusTransformer, orderStatusTransformer, paymentTypeTransformer)
+                    .transform(sourceOrder, Order.class); 
+        }
+
+        @Override
+        public DestinationCode convert(SourceCode sourceCode) {
+            return beanUtils.getTransformer().transform(sourceCode, DestinationCode.class);  
+        }
+    }
